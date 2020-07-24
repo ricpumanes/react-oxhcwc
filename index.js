eca81b0a15a4542f210fec1ac1bff6a38a0fa862
ricpumanes <ricpumanes@gmail.com>
2020-07-24 19:28:50 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+import Slider from './components/Slider';
+
+const videoOptions = {
+  fluid: true,
+  autoplay: false,
+  controls: false,
+  responsive: true,
+  preload: "none",
+  sources: [
+    {
+      src: "https://media.w3.org/2010/05/sintel/trailer_hd.mp4",
+      type: "video/mp4"
+    }
+  ]
+};
+
+const dummyVideos = Array.from({ length: 15 }, (i, j) => ({
+  ...videoOptions,
+  id: "dummy-video-" + j
+}));
+
+const App = () => {
+   return (
+      <div>
+        <Slider />
+      </div>
+    );
+}
+
+render(<App />, document.getElementById('root'));
